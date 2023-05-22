08c15d3afe0c250b1aded0fd9da0da32a3c7b2a6
Kyle Caprio <capriok7@gmail.com>
2023-05-22 09:55:19 -0700

    generateMetadata

--- src/app/(app)/profile/[guid]/page.tsx
+++ src/app/(app)/profile/[guid]/page.tsx
@@ -4,7 +4,7 @@ import useAuthUser from '~/utils/useAuthUser'
 import BannedBanner from './components/BannedBanner'
 import { RiderProfile } from './components/RiderProfile'
 
-export async function generatemetadata({ params }) {
+export async function generateMetadata({ params }) {
   const rider = await Api.GetRider(params.guid)
 
   return {
