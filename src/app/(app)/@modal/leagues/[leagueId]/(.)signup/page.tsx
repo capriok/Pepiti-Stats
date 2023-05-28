478a7feb4a595232e324ee0a7c94263e862653f5
Kyle Caprio <capriok7@gmail.com>
2023-05-28 10:58:04 -0700

    league signup props

--- src/app/(app)/@modal/leagues/[leagueId]/(.)signup/page.tsx
+++ src/app/(app)/@modal/leagues/[leagueId]/(.)signup/page.tsx
@@ -1,14 +1,24 @@
 'use client'
 
-import InterceptingModal from '../../../InterceptingModal'
-import LeagueSignupForm from '~/app/(app)/leagues/_components/LeagueSignupForm'
+import { GetConstantOEMBikes, GetConstantServers } from "~/api"
+import getAuthUser from "~/api/getAuthUser"
+import InterceptingModal from "../../../InterceptingModal"
+import LeagueSignupForm from "~/app/(app)/leagues/_components/LeagueSignupForm"
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic"
 
 export default async function Page({ params: { leagueId } }) {
+  const user = await getAuthUser()
+  const oems = await GetConstantOEMBikes(user.token)
+  const servers = await GetConstantServers(user.token)
+
   return (
     <InterceptingModal>
-      <LeagueSignupForm leagueId={leagueId} />
+      <LeagueSignupForm
+        leagueId={leagueId}
+        bikes={Object.keys(oems).map((key) => ({ name: key, bikes: oems[key] }))}
+        servers={servers}
+      />
     </InterceptingModal>
   )
 }
