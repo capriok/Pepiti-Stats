1502829d8fff66bcf77a76c06212bbaa475b716c
Paul Richan <75490402+paulrichan@users.noreply.github.com>
2023-06-02 11:08:33 -0700

    added themed images and ThemedImage component

---
+++ src/components/ThemedImage.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useTheme } from "next-themes"
+import Image, { ImageProps } from "next/image"
+import { useEffect, useMemo, useState } from "react"
+
+export function ThemedImage(props: ImageProps & { alt: string }) {
+  const { theme } = useTheme()
+  const [imageSrc, setImageSrc] = useState(props.src)
+
+  const isLight = theme === "light"
+  const dotIdx = props.src.toString().indexOf(".")
+  const baseUrl = props.src.toString().slice(0, dotIdx - 2)
+  const urlExtension = props.src.toString().slice(dotIdx)
+  const themedImgUrl = baseUrl + (isLight ? "-l" : "-d") + urlExtension
+
+  useEffect(() => {
+    setImageSrc(themedImgUrl)
+  }, [themedImgUrl])
+
+  // fail safe if we accidentally use an img that isn't theme ready
+  function handleError() {
+    setImageSrc(props.src)
+  }
+
+  return <Image {...props} src={imageSrc} onError={handleError} alt={props.alt} />
+}
